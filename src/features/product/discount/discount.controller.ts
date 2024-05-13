d9fdf94d5623cd24575c379ca3feee6e97d0135e
kaizen <razipurali@gmail.com>
2024-05-13 09:37:49 +0330

    Added discount module

--- src/features/product/discount/discount.controller.ts
+++ src/features/product/discount/discount.controller.ts
@@ -1,90 +1,21 @@
-import { CreateDiscountDto } from "./dto/create.dto";
 import { DiscountService } from "./discount.service";
-import { EditDiscountDto } from "./dto/edit.dto";
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Post,
-  Put,
-  Query,
-  Req,
-  UseGuards,
-} from "@nestjs/common";
+import { Controller, Get, Put, UseGuards } from "@nestjs/common";
 import { AccessLevel } from "src/helpers/decorators";
 import { HasAccessGuard } from "src/guards/access.guard";
-import { Request } from "express";
 
 @Controller("discount")
 export class DiscountController {
   constructor(private service: DiscountService) {}
 
-  @Get("/:complexId")
-  async findAll(
-    @Query() queryParams: { [props: string]: string },
-    @Param("complexId") complexId: string
-  ) {
-    return await this.service.findAll(complexId, queryParams);
+  @Get()
+  async findAll() {
+    return await this.service.findAll();
   }
 
-  @Get("/:complexId/:id")
-  async findById(@Param("id") complexId: string) {
-    return await this.service.findById(complexId);
-  }
-
-  @Post("/:complexId")
+  @Put()
   @AccessLevel([1, 2])
   @UseGuards(HasAccessGuard)
-  async create(
-    @Body() body: CreateDiscountDto,
-    @Param("complexId") complexId: string,
-    @Req() req: Request
-  ) {
-    const theUser = req.currentUser;
-    return await this.service.create({
-      ...body,
-      complex: complexId,
-      author_id: theUser._id.toString(),
-    });
-  }
-
-  @Put("/:complexId/:id")
-  @AccessLevel([1, 2])
-  @UseGuards(HasAccessGuard)
-  async editItem(
-    @Param("id") recordId: string,
-    @Body() body: EditDiscountDto,
-    @Req() req: Request
-  ) {
-    const theUser = req.currentUser;
-    return await this.service.findAndEdit({
-      id: recordId,
-      body,
-      author_id: theUser._id.toString(),
-    });
-  }
-
-  @Put("/:complexId/toggle/:id")
-  @AccessLevel([1, 2])
-  @UseGuards(HasAccessGuard)
-  async toggleActivation(@Param("id") recordId: string, @Req() req: Request) {
-    const theUser = req.currentUser;
-    return await this.service.toggleActivation({
-      id: recordId,
-      author_id: theUser._id.toString(),
-    });
-  }
-
-  @Delete("/:complexId/:id")
-  @AccessLevel([1, 2])
-  @UseGuards(HasAccessGuard)
-  async deleteItem(@Param("id") recordId: string, @Req() req: Request) {
-    const theUser = req.currentUser;
-    return await this.service.deleteOne({
-      id: recordId,
-      author_id: theUser._id.toString(),
-    });
+  async updateData() {
+    return await this.service.updateData();
   }
 }
--- src/features/product/discount/discount.service.ts
+++ src/features/product/discount/discount.service.ts
@@ -1,181 +1,58 @@
 import { Cron, CronExpression } from "@nestjs/schedule";
 import { DiscountDocument } from "./discount.schema";
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { toObjectId } from "src/helpers/functions";
-import { ActivityService } from "src/features/complex/activities/activities.service";
-import { messages } from "src/helpers/constants";
+import { sofreBaseUrl } from "src/helpers/constants";
+import { HttpService } from "@nestjs/axios";
+import { lastValueFrom } from "rxjs";
 
 @Injectable()
 export class DiscountService {
   constructor(
     @InjectModel("discount")
     private readonly model: Model<DiscountDocument>,
-    private readonly logService: ActivityService
+    private readonly httpService: HttpService
   ) {}
 
-  async findAll(complex: string, queryParams?: { [props: string]: string }) {
-    const {
-      limit = "30",
-      page = "1",
-      sort,
-      direction = "asc",
-    } = queryParams || {};
-
-    const sortObj = {};
-    if (sort) sortObj[sort] = direction === "asc" ? -1 : 1;
-
-    const results = await this.model
-      .find({ complex })
-      .sort(sortObj)
-      .limit(parseInt(limit))
-      .skip((parseInt(page) - 1) * parseInt(limit))
-      .populate("folders products")
-      .select("-complex")
-      .exec();
-
-    const totalDocuments = await this.model
-      .find({ complex })
-      .countDocuments()
-      .exec();
-    const numberOfPages = Math.ceil(totalDocuments / parseInt(limit));
-
-    return {
-      items: results,
-      numberOfPages,
-    };
+  async findAll() {
+    return await this.model.find().exec();
   }
 
-  async findAllComplexDiscounts(complex: string) {
-    return await this.model.find({ complex }).exec();
+  async updateData() {
+    const res = await lastValueFrom(
+      this.httpService.get(
+        `${sofreBaseUrl}/discount/localdb/${process.env.COMPLEX_ID}`
+      )
+    );
+    for await (const record of res.data) {
+      const modifiedResponse = res.data.map((item) => {
+        const productsObjectIds = (item.products || []).map((p) =>
+          toObjectId(p)
+        );
+        const foldersObjectIds = (item.folders || []).map((f) => toObjectId(f));
+        return {
+          ...item,
+          products: productsObjectIds || [],
+          folders: foldersObjectIds || [],
+          _id: toObjectId(item._id),
+          complex: toObjectId(item.complex),
+        };
+      });
+      await this.model.updateMany(
+        { _id: record._id },
+        { $set: modifiedResponse },
+        { upsert: true }
+      );
+    }
   }
 
-  async findAllActiveComplexDiscounts(complex_id: string) {
-    return await this.model
-      .find({
-        $and: [
-          { complex: toObjectId(complex_id) },
-          { is_active: true },
-          { start_date: { $lt: new Date() } },
-          { expiration_date: { $gt: new Date() } },
-        ],
-      })
-      .lean()
-      .exec();
-  }
-
-  async findById(id: string) {
-    return await this.model
-      .findById(id)
-      .populate("folders")
-      .populate("products")
-      .exec();
-  }
-
-  async create(data: {
-    complex: string;
-    percent: number;
-    start_date: Date;
-    expiration_date: Date;
-    type: 1 | 2 | 3;
-    products: string[];
-    folders: string[];
-    author_id: string;
-  }) {
-    const {
-      complex,
-      percent,
-      start_date,
-      expiration_date,
-      type,
-      products,
-      folders,
-      author_id,
-    } = data || {};
-    const newRecord = new this.model({
-      percent,
-      start_date,
-      expiration_date,
-      complex: toObjectId(complex),
-      type,
-      products: type === 3 ? products : [],
-      folders: type === 2 ? folders : [],
-    });
-    await this.logService.create({
-      type: 1,
-      description: `${percent} درصد تخفیف اضافه شد.`,
-      complex_id: complex,
-      user_id: author_id,
-      dist: newRecord,
-      dist_type: "discount",
-    });
-    return await newRecord.save();
-  }
-
-  async findAndEdit(data: {
-    id: string;
-    body: {
-      percent: number;
-      start_date: Date;
-      expiration_date: Date;
-    };
-    author_id: string;
-  }) {
-    const { author_id, body, id } = data;
-    const { percent, start_date, expiration_date } = body || {};
-    const theRecord = await this.model.findById(id);
-    if (!theRecord) throw new NotFoundException();
-    theRecord.percent = percent;
-    theRecord.start_date = start_date;
-    theRecord.expiration_date = expiration_date;
-    await this.logService.create({
-      type: 2,
-      description: "تخفیف ویرایش شد.",
-      complex_id: theRecord.complex._id.toString(),
-      user_id: author_id,
-      dist: theRecord,
-      dist_type: "discount",
-    });
-    return await theRecord.save();
-  }
-
-  async toggleActivation(data: { id: string; author_id: string }) {
-    const { author_id, id } = data;
-    const theRecord = await this.model.findById(id);
-    if (!theRecord) throw new NotFoundException();
-    const newVal = !theRecord.is_active;
-    theRecord.is_active = newVal;
-    await this.logService.create({
-      type: 2,
-      description: `تخفیف ${newVal ? "فعال" : "غیرفعال"} شد.`,
-      complex_id: theRecord.complex._id.toString(),
-      user_id: author_id,
-      dist: theRecord,
-      dist_type: "discount",
-    });
-    return await theRecord.save();
-  }
-
-  async deleteOne(data: { id: string; author_id: string }) {
-    const { author_id, id } = data;
-    const theRecord = await this.model.findById(id);
-    if (!theRecord) throw new NotFoundException(messages[404]);
-    await this.logService.create({
-      type: 3,
-      description: "تخفیف حذف شد.",
-      complex_id: theRecord.complex._id.toString(),
-      user_id: author_id,
-    });
-    await this.model.deleteOne({ _id: id });
-    return "success";
-  }
-
-  @Cron(CronExpression.EVERY_DAY_AT_1AM, {
-    name: "product-discounts-cron",
+  @Cron(CronExpression.EVERY_DAY_AT_11AM, {
+    name: "complex-update-cron",
     timeZone: "Asia/Tehran",
   })
   async handleCron() {
-    await this.model.deleteMany({ expiration_date: { $lte: new Date() } });
+    await this.updateData();
   }
 }
