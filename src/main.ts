82afdd7ce4d675f642afc507456889011fd7e62b
kaizenjs <reazipurali@gmail.com>
2024-05-21 13:31:22 +0330

    Tried to make it https

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from "./app.module";
 import helmet from "helmet";
 import * as compression from "compression";
 import { ValidationPipe } from "@nestjs/common";
+import * as fs from "fs";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync("./src/cert/key.pem"),
+    cert: fs.readFileSync("./src/cert/cert.pem"),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   app.use(compression());
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
