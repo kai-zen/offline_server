066b16d3be803f75d05c80ec109f96c99804df55
kaizenjs <reazipurali@gmail.com>
2024-07-19 19:28:04 +0330

    Removed all privacy of socket

--- src/websocket/events.gateway.ts
+++ src/websocket/events.gateway.ts
@@ -1,5 +1,3 @@
-import { AccessService } from "src/features/user/access/access.service";
-import { JwtService } from "@nestjs/jwt";
 import {
   MessageBody,
   SubscribeMessage,
@@ -11,31 +9,20 @@ import { OrderDocument } from "src/features/order/order.schema";
 
 @WebSocketGateway({ cors: { origin: "*" } })
 export class EventsGateway {
-  constructor(
-    private readonly jwtService: JwtService,
-    private readonly accessService: AccessService
-  ) {}
+  constructor() {}
 
   @WebSocketServer()
   server: Server;
 
   async handleConnection() {
     this.server.on("connection", async (socket) => {
-      const { localToken, complex_id } = socket.handshake.query;
-      if (complex_id && localToken) {
-        const tokenData = await this.jwtService.verifyAsync(
-          localToken as string,
-          { secret: "POKp6aK2DpGViU2wVvHYb3q00tDn5b" }
-        );
-        const theAccess = await this.accessService.hasAccess(tokenData._id);
-        if (theAccess) socket.join("local-orders-channel");
-      }
+      socket.join("local-orders-channel");
     });
   }
 
   async addOrder(orderData: OrderDocument) {
     this.server.to("local-orders-channel").emit("local-live-orders", {
-      ...orderData,
+      ...(orderData?.toObject ? orderData.toObject() : orderData),
       is_update: false,
     });
   }
