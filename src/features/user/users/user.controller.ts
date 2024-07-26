495daeeb6f1d4dfe68b1f3ba2d2dc95d16f65d7d
kaizenjs <reazipurali@gmail.com>
2024-07-26 09:30:03 +0330

    Fixed users routes

--- src/features/user/users/user.controller.ts
+++ src/features/user/users/user.controller.ts
@@ -1,28 +1,59 @@
 import { UserService } from "./user.service";
 import { Controller, Get, Param, Put, Query } from "@nestjs/common";
 
-@Controller("user")
+@Controller()
 export class UserController {
   constructor(private service: UserService) {}
 
-  @Get()
+  @Get("/user")
   async findAll(@Query() queryParams: { [props: string]: string }) {
     return await this.service.findAll(queryParams);
   }
 
-  @Get("/is-running") isServerRunning() {
+  @Get("/user/:complexId")
+  async findAllByComplex(@Query() queryParams: { [props: string]: string }) {
+    return await this.service.findAll(queryParams);
+  }
+
+  @Get("/complex-user")
+  async findAll2(@Query() queryParams: { [props: string]: string }) {
+    return await this.service.findAll(queryParams);
+  }
+
+  @Get("/complex-user/:complexId")
+  async findAllByComplex2(@Query() queryParams: { [props: string]: string }) {
+    return await this.service.findAll(queryParams);
+  }
+
+  @Get("/user/is-running") isServerRunning() {
     return {
       is_connected: true,
     };
   }
 
-  @Get("/:id")
+  @Get("/complex-user/is-running") isServerRunning2() {
+    return {
+      is_connected: true,
+    };
+  }
+
+  @Get("/user/:id")
   async findById(@Param("id") complexId: string) {
     return await this.service.findById(complexId);
   }
 
-  @Put()
+  @Get("/complex-user/:id")
+  async findById2(@Param("id") complexId: string) {
+    return await this.service.findById(complexId);
+  }
+
+  @Put("/user")
   async updateData() {
+    return await this.service.updateData();
+  }
+
+  @Put("/complex-user")
+  async updateData2() {
     return await this.service.updateData();
   }
 }
